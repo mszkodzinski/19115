2ac716bf911295959d5abe36a80c414da313d5a0
Michał Szkodziński <michal@szkodzinski.pl>
2014-03-14 22:06:23 +0100

    struktura

---
+++ config/default.php
@@ -0,0 +1,15 @@
+<?php
+return $config = array(
+    'db' => array(
+        'host' => 'localhost',
+        'port' => '3306',
+        'login' => '',
+        'pass' => ''
+    ),
+    'mail' => array(
+        'host' => 'imap.gmail.com',
+        'port' => '587',
+        'login' => '',
+        'pass' => ''
+    )
+);
