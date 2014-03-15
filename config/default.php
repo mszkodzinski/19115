028d200bdfb42d70e452cc31072808202c849d2e
Michał Szkodziński <michal@szkodzinski.pl>
2014-03-15 12:41:43 +0100

    csv

--- config/default.php
+++ config/default.php
@@ -3,8 +3,8 @@ return $config = array(
     'db' => array(
         'host' => 'localhost',
         'port' => '3306',
-        'login' => '',
-        'pass' => ''
+        'login' => '19115',
+        'pass' => '19115'
     ),
     'mail' => array(
         'host' => 'imap.gmail.com',
--- public/index.php
+++ public/index.php
@@ -3,4 +3,10 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-phpinfo();
+
+require_once('../application/DB.php');
+
+$db = new DB();
+
+//echo __FILE__;
+//phpinfo();
