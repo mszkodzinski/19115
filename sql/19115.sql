d3efa0978cbc3bcbab5cb139491d330b282a906f
GlockPL <pakerzus@tlen.pl>
2014-03-15 15:30:11 +0100

    Poprawki w szkielecie bazy.

--- sql/19115.sql
+++ sql/19115.sql
@@ -80,9 +80,10 @@ CREATE TABLE IF NOT EXISTS `organization` (
 --
 
 CREATE TABLE IF NOT EXISTS `source` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
