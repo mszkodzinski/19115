4cb9bfa9e64b7daa7bec63bf33fe1b8473559626
Michał Szkodziński <michal@szkodzinski.pl>
2014-03-15 16:34:37 +0100

    api getdata

--- library/Api/Engine.php
+++ library/Api/Engine.php
@@ -24,15 +24,41 @@ class Api_Engine
 
     public function getData($params)
     {
+        $r = new Api_Responce();
         foreach (array('filter', 'groupby', 'sortby', 'order') as $key) {
             if (!isset($params[$key])) {
                 $params[$key] = null;
             }
         }
+
+        if (!$params['groupby']) {
+            $r->status = false;
+            $r->error = array(
+                'groupby' => 'brak parametru'
+            );
+            return $r;
+        } else {
+            $map = array(
+                'status' => 'k_status',
+                'organization' => 'k_organization',
+                'district' => 'district',
+                'year' => 'year',
+                'month' => 'month',
+                'year_month' => 'year_month'
+            );
+            if (!isset($map[$params['groupby']])) {
+                $r->status = false;
+                $r->error = array(
+                    'groupby' => 'zla wartosc'
+                );
+                return $r;
+            }
+            $params['groupby'] = $map[$params['groupby']];
+        }
+
         $reader = new Reader_Data();
         $result = $reader->getData($params);
 
-        $r = new Api_Responce();
         if (!$result) {
             $r->status = false;
             $r->code = 500;
