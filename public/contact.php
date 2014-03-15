825b6ca1befcdbe7920fd748b6edb55d1c0d7383
Pawel <p.u.kaczmarek@gmail.com>
2014-03-15 13:24:53 +0100

    bootstrap template

---
+++ public/contact.php
@@ -0,0 +1,16 @@
+<?php
+include_once "header.inc";
+?>
+
+
+    <div class="container">
+        <div class="page-header" id="banner">
+            <div class="row">
+                <h1>Contact Us</h1>
+            </div>
+        </div>
+    </div>
+
+<?php
+include_once "footer.inc";
+?>
--- public/index.php
+++ public/index.php
@@ -1,2 +1,35 @@
 <?php
-phpinfo();
+include_once "header.inc";
+?>
+
+
+<div class="container">
+    <div class="page-header" id="banner">
+        <div class="row">
+            <div class="col-lg-6">
+                <h1>Warszawa 19115</h1>
+                <p class="lead">Wizualizcja danych</p>
+            </div>
+            <div class="col-lg-6" style="padding: 15px 15px 0 15px;">
+                <div class="well sponsor">
+                    <a href="./charts.php">
+                <span style="float: left; margin-right: 15px;">
+                  <img src="" width="175" height="135">
+                </span>
+                    </a>
+                    <a href="./charts.php"  >
+                        <h4 style="margin-bottom: 0.4em;">Wizualizcja danych</h4>
+                        <div class="clearfix">
+                            <p>Zobacz wszystko na mapie</p>
+                            <p>Dane z Warszawy!</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include_once "footer.inc";
+?>
