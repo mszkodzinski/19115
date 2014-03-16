2f04a245984b356ca173dc045b9982ced7e70723
Michał Szkodziński <michal@szkodzinski.pl>
2014-03-16 11:37:39 +0100

    s

---
+++ config/default.php
@@ -0,0 +1,16 @@
+<?php
+return $config = array(
+    'db' => array(
+        'host' => 'localhost',
+        'port' => '3306',
+        'login' => 'root',
+        'pass' => 'user2014',
+        'dbname' => '19115'
+    ),
+    'mail' => array(
+        'host' => 'imap.gmail.com',
+        'port' => '993',
+        'login' => '19115@epf.org.pl',
+        'pass' => 'fundacja23'
+    )
+);
