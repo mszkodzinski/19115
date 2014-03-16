3fcfc64e72d4e90d317a53035dee8969417ba34b
Michał Szkodziński <michal@szkodzinski.pl>
2014-03-16 14:27:03 +0100

    Merge branch 'master' of https://github.com/mszkodzinski/19115
    Conflicts:
    	public/imap.php

--- sql/19115.sql
+++ sql/19115.sql
@@ -3,13 +3,12 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Czas wygenerowania: 15 Mar 2014, 14:19
+-- Czas wygenerowania: 16 Mar 2014, 14:26
 -- Wersja serwera: 5.5.35
 -- Wersja PHP: 5.3.10-1ubuntu3.10
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -24,6 +23,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktura tabeli dla  `district`
+--
+
+CREATE TABLE IF NOT EXISTS `district` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_district` (`name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=22 ;
+
+-- --------------------------------------------------------
+
+--
 -- Struktura tabeli dla  `importer`
 --
 
@@ -33,7 +45,7 @@ CREATE TABLE IF NOT EXISTS `importer` (
   `import_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `status` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=17 ;
 
 -- --------------------------------------------------------
 
@@ -45,21 +57,23 @@ CREATE TABLE IF NOT EXISTS `notification` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `number` varchar(255) NOT NULL COMMENT 'Numer zgłoszenia',
   `k_status` int(11) NOT NULL,
-  `date_of_acceptance` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'Data przyjęcia zgłoszenia',
-  `close_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `date_of_acceptance` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'Data przyjęcia zgłoszenia',
+  `close_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `date` datetime NOT NULL,
   `k_source` int(11) NOT NULL,
   `k_organization` int(11) NOT NULL,
-  `lattitude` float NOT NULL,
-  `longtitude` float NOT NULL,
-  `district` int(11) NOT NULL,
+  `lattitude` varchar(255) NOT NULL,
+  `longtitude` varchar(255) NOT NULL,
+  `k_district` int(11) NOT NULL,
   `street` varchar(255) NOT NULL,
   `house_nr` varchar(10) NOT NULL,
   `notification_time` varchar(32) NOT NULL,
   `import_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `lattitude_2000` varchar(255) NOT NULL,
+  `longtitude_2000` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `uniq_number` (`number`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=912 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6243 ;
 
 -- --------------------------------------------------------
 
@@ -69,11 +83,11 @@ CREATE TABLE IF NOT EXISTS `notification` (
 
 CREATE TABLE IF NOT EXISTS `organization` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL,
-  `description` varchar(32),
+  `name` int(11) NOT NULL,
+  `description` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `uniq_organisation` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8436 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 -- --------------------------------------------------------
 
@@ -86,7 +100,7 @@ CREATE TABLE IF NOT EXISTS `source` (
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `uniq_status` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8435 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
 -- --------------------------------------------------------
 
@@ -99,7 +113,7 @@ CREATE TABLE IF NOT EXISTS `status` (
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `uniq_name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8435 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
